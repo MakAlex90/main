ae711b9d63b592fbe825113124c7d94b501abc0b
MakAlex90 <meskalin4ik9@gmail.com>
2024-08-09 17:29:59 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,5 @@
+number_of_homework = 12
+number_of_hours = 1.5
+name_curse = "Python"
+time_for_one_task = number_of_hours / number_of_homework
+print("Курс:", name_curse, "всего задач", number_of_homework, "затраченно часов:", number_of_hours, "среднее время выполнения", time_for_one_task )
