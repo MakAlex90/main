20d7cf6a3a44b47e244b14e0acf4eb99fb932bad
MakAlex90 <meskalin4ik9@gmail.com>
2024-08-09 13:29:13 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-# 1st program
-print(9 ** 0.5 * 5)
-# 2nd program
-print(9.99 > 9.98 and 1000 != 1000.1)
-# 3rd program
-print(2 * 2 + 2)
-print(2 * (2 + 2))
-print((2 * 2 + 2) == (2 * (2 + 2)))
-# 4th program
-print(int (float('123.456') * 10) % 10 )
+example = "Алексей"
+print(example[0])
+print(example[-1])
+print(example[3:])
+print(example[::-1])
+print(example[1:6:2])
