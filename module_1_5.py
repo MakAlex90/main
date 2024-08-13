c590df787349a2a24a008300fe32ebb8b241f9f8
MakAlex90 <meskalin4ik9@gmail.com>
2024-08-13 14:55:09 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -2,7 +2,7 @@ immutable_var = (1, 2, "a", "b", True)
 print (immutable_var)
 # immutable_var[2]= 4 # кортеж не поддерживает обращение по элементам, неизменяем.
 # print(immutable_var)
-mutable_list = ([1, 2, 3], ["a", "b", "c"], [True])
+mutable_list = [[1, 2, 3], ["a", "b", "c"], [True]]
 print(mutable_list)
 mutable_list[0] [0] = 5
 print(mutable_list)
